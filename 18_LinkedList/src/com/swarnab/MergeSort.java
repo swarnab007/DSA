381a17a090fefa87c2ae3f6cea384900ffe1bf31
swarnab007 <swarnabmessibanerjee@gmail.com>
2025-04-14 20:58:06 +0530

    Linked List updated

--- 18_LinkedList/src/com/swarnab/MergeSort.java
+++
@@ -1,7 +0,0 @@
-package com.swarnab;
-
-public class MergeSort {
-    public static void main(String[] args) {
-
-    }
-}
--- 18_LinkedList/src/com/swarnab/Questions.java
+++ 18_LinkedList/src/com/swarnab/Questions.java
@@ -1,9 +1,9 @@
 package com.swarnab;
 
-import java.util.List;
-
 public class Questions {
 
+    // Reverse the Linked List
+    // https://leetcode.com/problems/reverse-linked-list/description/
     public static ListNode reverse(ListNode head) {
         if(head == null) {
             return null;
@@ -22,6 +22,75 @@ public class Questions {
         head = prev;
         return head;
     }
+
+    // Merge Two Sorted Lists
+    // https://leetcode.com/problems/merge-two-sorted-lists/
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode head = new ListNode();
+        ListNode temp = head;
+
+        while(list1 != null && list2 != null) {
+            if(list1.val < list2.val) {
+                temp.next = new ListNode(list1.val);
+                list1 = list1.next;
+                temp = temp.next;
+            } else {
+                temp.next = new ListNode(list2.val);
+                list2 = list2.next;
+                temp = temp.next;
+            }
+        }
+
+        while(list1 != null) {
+            temp.next = new ListNode(list1.val);
+            list1 = list1.next;
+            temp = temp.next;
+        }
+
+        while(list2 != null) {
+            temp.next = new ListNode(list2.val);
+            list2 = list2.next;
+            temp = temp.next;
+        }
+
+        return head.next;
+    }
+
+    // Find the Middle Node
+    // https://leetcode.com/problems/middle-of-the-linked-list/
+    public ListNode middleNode(ListNode head) {
+        ListNode prev = null;
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        if(prev != null) {
+            prev.next = null;
+        }
+
+        return slow;
+    }
+
+    // Merge Sort in Linked Lists
+    // https://leetcode.com/problems/sort-list/
+    public ListNode sortList(ListNode head) {
+
+        if(head == null || head.next == null)
+            return head;
+
+        ListNode middle = middleNode(head);
+        ListNode left = sortList(head);
+        ListNode right = sortList(middle);
+
+        return mergeTwoLists(left, right);
+    }
+
+    //
 
     class ListNode {
         int val;
